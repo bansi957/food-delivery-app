8fc7ed6c61c1da4a7f9c40900c69b3caae49cf64
bansi957 <bansibasha@gmail.com>
2026-01-20 13:21:00 +0530

    Refactor Gmail transport configuration
    Removed the port and secure settings for Gmail transport.

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -2,8 +2,6 @@ const nodemailer=require("nodemailer")
 
 const transport=nodemailer.createTransport({
 service:"Gmail",
-  port: 465,
-  secure: true, // Use true for port 465, false for port 587
   auth: {
     user: process.env.MAIL_ID,
     pass: process.env.MAIL_PASS,}
