d7cc0e8faf33126d6f0a0b46e7573ee3ca683b9f
BANSI BASHA <bansibasha@gmail.com>
2026-01-27 17:49:44 +0530

    Update multer.js

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,13 +1,30 @@
-const multer=require('multer');
-const storage=multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,'./public')
-    },
+// const multer=require('multer');
+// const storage=multer.diskStorage({
+//     destination:function(req,file,cb){
+//         cb(null,'./public')
+//     },
 
-    filename:function(req,file,cb){
-        cb(null,Date.now()+'-'+file.originalname)
-    }
+//     filename:function(req,file,cb){
+//         cb(null,Date.now()+'-'+file.originalname)
+//     }
 
+// });
+
+// module.exports=multer({storage});
+
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadDir = "/tmp"; // ALWAYS exists on Render
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename(req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-module.exports=multer({storage});
+module.exports = multer({ storage });
