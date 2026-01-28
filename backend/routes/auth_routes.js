3f3188d58d9f4884c8e1565cb6871d9345791332
BANSI BASHA <bansibasha@gmail.com>
2026-01-28 11:41:32 +0530

    Update auth_routes.js

--- backend/routes/auth_routes.js
+++ backend/routes/auth_routes.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const authRouter=express.Router()
-const {signUp,signIn,signOut,sendotp, verifyOtp, resetPassword, googleAuth}=require("../controllers/auth_controller")
+const {signUp,signIn,signOut,sendotp, verifyOtp, resetPassword,googleLogin,googleSignup}=require("../controllers/auth_controller")
 
 
 authRouter.post("/signup",signUp);
@@ -10,5 +10,7 @@ authRouter.get("/signout",signOut)
 authRouter.post("/send-otp",sendotp);
 authRouter.post("/verify-otp",verifyOtp);
 authRouter.post("/change-password",resetPassword)
-authRouter.post("/googleAuth",googleAuth)
+authRouter.post("/googleAuth",googleSignup)
+authRouter.post("/googleAuth-signin",googleLogin)
+
 module.exports=authRouter
